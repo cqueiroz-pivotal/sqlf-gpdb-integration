1aa6e0e33b6983265373029aa53da2851d7cc88f
Carlos Queiroz <caxqueiroz@gmail.com>
2014-03-11 04:43:34 +0800

    ProxyDispatcher implemented.

--- src/main/java/com/gopivotal/poc/sqlfgpdb/MyProxyDispatcher.java
+++ src/main/java/com/gopivotal/poc/sqlfgpdb/MyProxyDispatcher.java
@@ -8,6 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -23,13 +26,90 @@ public class MyProxyDispatcher implements EventCallback {
     private HikariDataSource ds;
     private final Properties p = new Properties();
 
+    /** counter to choose next proxy table*/
+    private int rr = 1;
+
     @Override
     public void onEvent(Event event) throws SQLException {
 
+        ResultSet rs = event.getNewRowsAsResultSet();
+        ResultSet pkRS = event.getPrimaryKeysAsResultSet();
+        int numCols = rs.getMetaData().getColumnCount();
+
+        Connection conn = null;
+        PreparedStatement pstm = null;
+
+        try{
+            conn = ds.getConnection();
+
+            switch (event.getType()) {
+                case AFTER_INSERT:
+                case AFTER_UPDATE:
+                    processRow(rs, pkRS, numCols, conn);
+                    break;
+
+                case BULK_INSERT:{
+
+                    while(rs.next()){
+                        processRow(rs, pkRS, numCols, conn);
+                    }
+                    break;
+
+                }
+
+
+                case AFTER_DELETE:
+                    break;
+
+                default:
+                    break;
+            }
+
+        }catch (SQLException e){
+            LOGGER.error("Error updating table:",e);
+            throw new SQLException(e);
+
+        }finally{
+
+
+            if(conn!=null){
+                conn.close();
+            }
+        }
+
+
     }
+
+    private void processRow(ResultSet rs, ResultSet pkRS, int numCols, Connection conn) throws SQLException {
+        int proxyLocation;
+        String tableName;
+        PreparedStatement pstm;
+        StringBuffer sb = new StringBuffer();
+        for(int i = 1; i <= numCols ; i++){
+            sb.append(rs.getObject(i));
+            sb.append('|');
+        }
+        if(pkRS!=null){
+            pkRS.next();
+            Object pk = pkRS.getObject(1);
+            proxyLocation = (pk.hashCode() % Integer.parseInt(p.getProperty("numproxies"))) + 1;
+            tableName = p.getProperty("proxyTablePrefix") + "_" + proxyLocation;
+
+        }else{
+            proxyLocation = (rr++ % Integer.parseInt(p.getProperty("numproxies"))) + 1;
+            tableName = p.getProperty("proxyTablePrefix") + "_" + proxyLocation;
+        }
+
+        pstm = conn.prepareStatement("update " + tableName + " set value=? where k=1");
+        pstm.setString(1, sb.toString());
+        pstm.executeUpdate();
+        pstm.close();
+    }
+
 
     @Override
     public void close() throws SQLException {
+        ds.shutdown();
 
     }
 
@@ -50,11 +130,13 @@ public class MyProxyDispatcher implements EventCallback {
             HikariConfig config = new HikariConfig();
             config.setMinimumPoolSize(32);
             config.setMaximumPoolSize(64);
-            config.setDataSourceClassName("org.postgresql.ds.PGSimpleDataSource");
+            config.setDataSourceClassName("com.vmware.sqlfire.internal.jdbc.ClientDataSource");
             config.setPoolName("sqlfCP");
             config.addDataSourceProperty("url", p.getProperty("connectionURL"));
             config.addDataSourceProperty("user", p.getProperty("username"));
             config.addDataSourceProperty("password", p.getProperty("password"));
+
+            ds = new HikariDataSource(config);
 
 
         } catch (IOException e) {
